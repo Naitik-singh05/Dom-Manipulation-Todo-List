860a6d8cd497646d1f19c603a0fd1935a2394451
Naitik-singh05 <naitikthakur891@gmail.com>
2024-09-20 18:07:23 +0530

    Delete operator directory

--- operator/shortcircuting.js
+++
@@ -1,9 +0,0 @@
-console.log(10 && 30);
-console.log(0 && 30);
-console.log(10 && null);
-console.log("" && -0);
-
-console.log(10 || 30);
-console.log(0 || 30);
-console.log(10 || null);
-console.log("" || -0);
